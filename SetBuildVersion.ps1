dc98f9db588d7f8aadc9ea6492bc0603e6a8409c
tom-englert <mail@tom-englert.de>
2017-04-22 19:32:25 +0200

    Fix version script

--- SetBuildVersion.ps1
+++ SetBuildVersion.ps1
@@ -1,4 +1,4 @@
 (new-object Net.WebClient).DownloadString("https://raw.github.com/tom-englert/BuildScripts/master/BuildScripts.ps1") | iex
 
-Source-SetBuildVersion ContracsReSharperInterop\Properties\AssemblyInfo.cs
-Vsix-SetBuildVersion ContracsReSharperInterop.Vsix\source.extension.vsixmanifest
+Source-SetBuildVersion "ContracsReSharperInterop\Properties\AssemblyInfo.cs"
+Vsix-SetBuildVersion "ContracsReSharperInterop.Vsix\source.extension.vsixmanifest"
